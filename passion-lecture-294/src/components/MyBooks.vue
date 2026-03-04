4b62f29a728cd4f404e86626e20d7ee0b91a3b46
Aaron Vichery <pa84igb@eduvaud.ch>
2026-03-04 08:41:00 +0100

    feat(MyBooks.vue):Remettre le bouton pour ajouter des livres

--- passion-lecture-294/src/components/MyBooks.vue
+++ passion-lecture-294/src/components/MyBooks.vue
@@ -29,12 +29,20 @@
 
           <a v-for="author in authors" :key="author.id" v-show="book.authorId == author.id">
             {{ author.firstName }} {{ author.lastName }}
+            
           </a>
 
           <a class="user-tag" v-for="user in users" v-show="user.id == book.userId">@{{ user.pseudo }}</a>
+          <div>
+            <img src="../assets/pinceau.png" />
+            <img src="../assets/poubelle.png" />
+          </div>
         </div>
       </RouterLink>
     </div>
+    <RouterLink to="/addbooks">
+    <button>Ajouter</button>
+  </RouterLink>
 
     <p v-if="filteredBooks.length === 0" style="color: #4b5fa9">Aucun livre trouvé.</p>
   </div>
@@ -171,6 +179,59 @@ nav a.active {
 .user-tag {
   font-size: 11px !important;
   opacity: 0.8;
+}
+
+.infos div {
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+  margin-top: 10px;
+  padding-top: 8px;
+  border-top: 1px solid rgba(255, 255, 255, 0.2);
+}
+
+/* Les icônes moins grosses (Pinceau et Poubelle) */
+.infos div img {
+  width: 30px;  /* Taille encore plus petite pour la discrétion */
+  height: 30px;
+  cursor: pointer;
+  filter: brightness(0) invert(1);
+  transition: 0.2s;
+}
+
+.infos div img:hover {
+  transform: scale(1.2);
+  opacity: 0.7;
+}
+
+/* --- LE GROS BOUTON AJOUTER (TOUT EN BAS) --- */
+/* On cible le RouterLink qui entoure le bouton pour le forcer à aller à la ligne */
+.livres > a {
+  width: 100%; 
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+}
+
+button {
+  background-color: #4b5fa9;
+  color: white;
+  border: none;
+  padding: 22px 80px;      /* Très gros bouton */
+  border-radius: 50px;
+  font-size: 1.3rem;
+  font-weight: 900;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin:0;          /* Espace au-dessus et en dessous */
+  box-shadow: 0 rgba(75, 95, 169, 0.3);
+  transition: all 0.3s ease;
+}
+
+button:hover {
+  background-color: #354685;
+  transform: scale(1.05);
+  box-shadow: 0 15px 25px rgba(75, 95, 169, 0.4);
 }
 
 .footer {
