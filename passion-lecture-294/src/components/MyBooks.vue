3e51961cc3f6c86ad6dc177bb567cd88d925d518
Aaron Vichery <pa84igb@eduvaud.ch>
2026-03-04 09:25:17 +0100

    feat(image-livre):Changer les chemin de l'image

--- passion-lecture-294/src/components/MyBooks.vue
+++ passion-lecture-294/src/components/MyBooks.vue
@@ -23,7 +23,7 @@
   <div class="livres">
     <div class="livre" v-for="book in filteredBooks" :key="book.id" v-show="book.userId == 1">
       <RouterLink :to="`/book/${book.id}`">
-        <img src="../assets/livres.png" />
+        <img :src="book.image" />
         <div class="infos">
           <a class="book-title">{{ book.title }}</a>
 
