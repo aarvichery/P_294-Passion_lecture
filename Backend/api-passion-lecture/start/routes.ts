cc3180b9709a026a6fb1a3a43ee3900a86ee7636
Elias <144105283+EviV2@users.noreply.github.com>
2026-05-05 13:34:53 +0200

    fd

--- Backend/api-passion-lecture/start/routes.ts
+++ Backend/api-passion-lecture/start/routes.ts
@@ -8,9 +8,37 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+
+const BooksController = () => import('#controllers/books_controller')
+const CategoriesController = () => import('#controllers/categories_controller')
+const AuthorsController = () => import('#controllers/authors_controller')
+const CommentsController = () => import('#controllers/comments_controller')
+const AuthController = () => import('#controllers/auth_controller')
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router
+  .group(() => {
+    router.resource('books', BooksController).apiOnly()
+    router.resource('categories', CategoriesController).apiOnly()
+    router.resource('authors', AuthorsController).apiOnly()
+    router
+      .group(() => {
+        router.resource('comments', CommentsController).apiOnly()
+      }).prefix("books/:book_id")
+
+
+    router 
+      .group(() => { 
+    router.post('register', [AuthController, 'register']) 
+    router.post('login', [AuthController, 'login']) 
+    router.post('logout', [AuthController, 'logout']) // .use(middleware.auth()) 
+  }) 
+  .prefix('user') 
+  })
+  .prefix('v1/api')
