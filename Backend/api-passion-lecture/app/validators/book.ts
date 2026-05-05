48a0baf98fa2e1525bf864a93a8aec783834c159
Elias <144105283+EviV2@users.noreply.github.com>
2026-05-05 15:42:49 +0200

    Finir validators

--- Backend/api-passion-lecture/app/validators/book.ts
+++ Backend/api-passion-lecture/app/validators/book.ts
@@ -3,11 +3,14 @@ import vine from '@vinejs/vine'
 const bookValidator = vine.compile(
   vine.object({
     title: vine.string().trim().minLength(2).maxLength(255),
-    nbPage: vine.number().positive(),
+    nbPage: vine.number().positive().withoutDecimals(),
     extract: vine.string().trim().optional(),
     resume: vine.string().trim().minLength(10),
     editor: vine.string().trim().minLength(2).maxLength(255),
     editionYear: vine.number().min(1000).max(new Date().getFullYear()),
+    userId: vine.number().positive().withoutDecimals(),
+    categoryId: vine.number().positive().withoutDecimals(),
+    authorId: vine.number().positive().withoutDecimals(),
   })
 )
 
