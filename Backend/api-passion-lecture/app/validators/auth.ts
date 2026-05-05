5ee34a229aee8649550dcaa1f1b514b7dbacc7f1
Elias <144105283+EviV2@users.noreply.github.com>
2026-05-05 14:45:13 +0200

    Finir les verficicators
    Auth one

--- Backend/api-passion-lecture/app/validators/auth.ts
+++ Backend/api-passion-lecture/app/validators/auth.ts
@@ -1,1 +1,24 @@
-import vine from '@vinejs/vine'
+import vine from '@vinejs/vine' 
+
+export const loginValidator = vine.compile( 
+  vine.object({ 
+    email: vine.string().email(), 
+    password: vine.string().minLength(8).maxLength(512), 
+  }) 
+) 
+
+export const registerValidator = vine.compile( 
+  vine.object({ 
+    firstname: vine.string().trim().minLength(2).maxLength(255), 
+    name: vine.string().trim().minLength(2).maxLength(255),
+    email: vine 
+      .string() 
+      .trim()
+      .email()     
+      .unique(async (query, field) => { 
+        const user = await query.from('users').where('email', field).first() 
+        return !user 
+      }), 
+    password: vine.string().minLength(8).maxLength(512), 
+  }) 
+)
