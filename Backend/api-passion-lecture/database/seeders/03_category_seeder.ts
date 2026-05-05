58ede1e3ef0377f6857303563fd16f242440b891
Elias <144105283+EviV2@users.noreply.github.com>
2026-05-05 17:20:39 +0200

    Feat: Modifier le seeder categories pour y inclure toutes les categories définitive

--- Backend/api-passion-lecture/database/seeders/03_category_seeder.ts
+++ Backend/api-passion-lecture/database/seeders/03_category_seeder.ts
@@ -4,10 +4,36 @@ import { BaseSeeder } from '@adonisjs/lucid/seeders'
 export default class extends BaseSeeder {
   async run() {
     await Category.createMany([
-      {
-        id: 1,
-        name: 'Enfant'
-      }
+
+
+{
+  id: 1,
+  name: "Documentaire"
+},
+{
+  id: 2,
+  name: "SCI-FI"
+},
+{
+  id: 3,
+  name: "Bande-Dessinée"
+},
+{
+  id: 4,
+  name: "Roman"
+},
+{
+  id: 5,
+  name: "Manga"
+},
+{
+  id: 6,
+  name: "Policier"
+},
+{
+  id: 7,
+  name: "Autre"
+}
     ])
   }
 }
