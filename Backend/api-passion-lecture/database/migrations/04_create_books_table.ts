c8899979f53883a330b412be76c84de22ba568be
Aaron Vichery <pa84igb@eduvaud.ch>
2026-05-05 13:32:09 +0200

    update(errors):Modifier pour éviter les erreurs avec les champs de la db

--- Backend/api-passion-lecture/database/migrations/04_create_books_table.ts
+++ Backend/api-passion-lecture/database/migrations/04_create_books_table.ts
@@ -8,16 +8,16 @@ export default class extends BaseSchema {
       table.increments('id')
 
       table.string('title').unique().notNullable()
-      table.integer('nbPage').notNullable()
+      table.integer('nb_page').notNullable()
       table.string('extract').notNullable()
       table.string('resume').notNullable()
       table.string('editor').notNullable()
-      table.integer('anneeEdition').notNullable()
+      table.integer('edition_year').notNullable()
       // table.image('image')
 
       table.integer('user_id').notNullable().unsigned().references('id').inTable('users').onDelete('CASCADE')
       table.integer('category_id').notNullable().unsigned().references('id').inTable('categories').onDelete('CASCADE')
-      table.integer('authors_id').notNullable().unsigned().references('id').inTable('authors').onDelete('CASCADE')
+      table.integer('author_id').notNullable().unsigned().references('id').inTable('authors').onDelete('CASCADE')
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
